dbd4a4f03e5ca5136b82ae4a97772330995376c9
devopsotrator <devopsotrator@imp.to>
2019-05-06 20:35:06 +0300

    gogo power puffy

--- runme.sh
+++ runme.sh
@@ -1,11 +1,42 @@
 !#/bin/ksh
 
-
 # выбивает сценарий на первой ошибке
 set -e
 
-doas pkg_add \
+local HOSTNAME
+HOSTNAME="hostname.kek.org"
+hostname $HOSTNAME
+echo $HOSTNAME > /etc/myname
+
+local DOMAIN
+DOMAIN="kek.org"
+domainname $DOMAIN
+echo $DOMAIN > /etc/defaultdomain
+
+# man doas
+# man doas.conf
+cp "/etc/examples/doas.conf" "/etc/doas.conf"
+
+pkg_add \
+	git \
 	jdk-11.0.2.9.3p0v0 \
+	scala \
 	elasticsearch \
 	kibana \
 	logstash \
+
+rcctl enable \
+	httpd \
+	unbound \
+	nsd \
+	elasticsearch \
+	kibana \
+	logstash
+
+rcctl restart \
+	httpd \
+	unbound \
+	nsd \
+	elasticsearch \
+	kibana \
+	logstash
